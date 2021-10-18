cc9c4e8313d7261284c1aca55c4b72a6859eb7a1
Keristero Kerivinix <Keristero@gmail.com>
2021-10-18 17:57:55 +1300

    wip touching up rick

--- mods/enemies/ultimate_warrior/character/entry.lua
+++ mods/enemies/ultimate_warrior/character/entry.lua
@@ -1,7 +1,8 @@
 
-local enemy_info = {
+local character_info = {
     name = "RickAstley",
-    hp = 100,
+    hp = 499,
+    height = 120
 }
 
 enraged = false
@@ -23,12 +24,17 @@ function package_init(self)
     fire_tower_animation_path = _modpath.."firetower.animation"
     fire_tower_texture_path = _modpath.."firetower.png"
     fire_tower_texture = Engine.load_texture(fire_tower_texture_path)
+    fire_tower_sound = Engine.load_audio(_modpath.."firetower.ogg")
+
+    impacts_animation_path = _modpath .. "impacts.animation"
+    impacts_texture_path = _modpath .. "impacts.png"
+    impacts_texture = Engine.load_texture(impacts_texture_path)
 
     --Set up character meta
-    self:set_name(enemy_info.name)
-    self:set_health(enemy_info.hp)
+    self:set_name(character_info.name)
+    self:set_health(character_info.hp)
     self:set_texture(self.texture, true)
-    self:set_height(30)
+    self:set_height(character_info.height)
     self:share_tile(false)
     self:set_explosion_behavior(32, 1.0, false)
     self:set_position(0, 0)
@@ -39,8 +45,10 @@ function package_init(self)
     self.ai_state = "spawning"
     self.ai_timer = 0
     self.ai_jumps = 0
+    self.combo_length = 0
     self.ai_target_jumps = math.random(2,3)
     self.frames_between_jumps = 10
+    self.frames_before_attack_start = 2
     
     enraged = false
 
@@ -50,27 +58,37 @@ function package_init(self)
         if character:get_health() < 500 then
             enraged = true
         end
+        character.ai_timer = character.ai_timer + 1
         --debug_print("original update_func called: "..character.ai_state)
         if character.ai_state == "idle" then
-            character.ai_timer = character.ai_timer + 1
             if character.ai_timer > character.frames_between_jumps then
-                local is_attacking = false
+                local moving_to_attack_position = false
                 character.ai_jumps = character.ai_jumps + 1
                 if character.ai_jumps >= character.ai_target_jumps then
-                    is_attacking = true
-                    character.ai_state = "attacking"
+                    moving_to_attack_position = true
+                    character.ai_state = "preparing_attack"
                 end
-                big_brute_teleport(character,is_attacking)
-                if is_attacking then
-                    local action = action_random_attack(character)
-                    character:card_action_event(action,ActionOrder.Voluntary)
-                    action.action_end_func = function ()
+                big_brute_teleport(character,moving_to_attack_position)
+                character.ai_timer = 0
+            end
+        end
+        if character.ai_state == "preparing_attack" then
+            if character.ai_timer > character.frames_before_attack_start then
+                local action = action_random_attack(character)
+                character:card_action_event(action,ActionOrder.Voluntary)
+                character.ai_state = "attacking"
+                action.action_end_func = function ()
+                    character.ai_timer = 0
+                    if enraged and character.combo_length < 3 then
+                        character.ai_state = "preparing_attack"
+                        character.combo_length = character.combo_length + 1
+                    else
+                        character.combo_length = 0
                         character.ai_state = "idle"
                         character.ai_jumps = 0
                         self.ai_target_jumps = math.random(4,5)
                     end
                 end
-                character.ai_timer = 0
             end
         end
     end
@@ -103,7 +121,7 @@ function package_init(self)
 end
 
 
-function big_brute_teleport(character,is_attacking)
+function big_brute_teleport(character,moving_to_attack_position)
     local field = character:get_field()
     local user_team = character:get_team()
     local target_list = field:find_characters(function(other_character)
@@ -131,7 +149,7 @@ function big_brute_teleport(character,is_attacking)
         if target_character_tile:y() == other_tile:y() then
             return true
         else
-            return not is_attacking
+            return not moving_to_attack_position
         end
         return true
     end)
@@ -141,158 +159,124 @@ function big_brute_teleport(character,is_attacking)
         target_tile = character:get_current_tile()
     end
     character:teleport(target_tile,ActionOrder.Immediate)
+    return target_tile
 end
 
 function action_random_attack(character)
-    local attack_actions = {action_line_attack,action_hellfire}
+    local attack_actions = {action_fire_tower_line,action_fire_tower_random}
     local chosen_one = attack_actions[math.random(#attack_actions)]
     return chosen_one(character)
 end
 
-function action_line_attack(character)
-    local action_name = "line attack"
-    debug_print('action '..action_name)
+function action_fire_tower_line(character)
+    local direction = character:get_facing()
+    local target_tiles = {}
+    for i = 1, 9, 1 do
+        local t1 = character:get_tile(direction,i)
+        if t1 then
+            target_tiles[#target_tiles+1] = t1
+        end
+    end
+    local attack_action = action_fire_tower_target_tiles(character,target_tiles,200)
+    return attack_action
+end
 
-    local action = Battle.CardAction.new(character,"IDLE")
+function action_fire_tower_random(character)
+    local direction = character:get_facing()
+    local field = character:get_field()
+    local target_tiles = {}
+    local current_tile_x = character:get_current_tile():x()
+    for x = 0, current_tile_x, 1 do
+        for y = 0, 4, 1 do
+            if math.random(1,3) == 3 then
+                local t1 = field:tile_at(x,y)
+                if t1 then
+                    target_tiles[#target_tiles+1] = t1
+                end
+            end
+        end
+    end
+    local attack_action = action_fire_tower_target_tiles(character,target_tiles,200)
+    return attack_action
+end
+
+function action_fire_tower_target_tiles(character,target_tiles,damage)
+    local action_name = "fire_tower_target_tiles"
+    debug_print('action ' .. action_name)
+
+    local action = Battle.CardAction.new(character, "ATTACK")
     action:set_lockout(make_sequence_lockout())
 
     action.execute_func = function(self)
-        debug_print('executing action '..action_name)
+        debug_print('executing action ' .. action_name)
         local step1 = Battle.Step.new()
         local step2 = Battle.Step.new()
 
+        --add a reference to this function to indicate that it can be canceled
         local actor = self:get_actor()
+        action.pre_attack_anim_started = false
         action.attack_anim_started = false
+        action.pre_attack_time_counter = 0
+        action.pre_attack_time = 0.5
         action.attack_time_counter = 0
-        action.attack_time = 2
-        if enraged then
-            action.attack_time = 0.9
-        end
-        action.attack_anim_2_started = false
-        action.attack_time_2_counter = 0
-        action.attack_2_time = 2
+        action.attack_time = 0.5
+        action.pre_attack_counter_time = 0.1
+        action.counter_enabled = false
+
+        action.warning_toggle_frames = 4
+        action.warning_toggle = false
+        action.warning_toggle_frames_elapsed = 0
+        action.target_tiles = target_tiles
 
         step1.update_func = function(self, dt)
-            --debug_print('action '..action_name..' step 2')
-            if not action.attack_anim_started then
+            -- debug_print('action '..action_name..' step 1')
+            if not action.pre_attack_anim_started then
                 local anim = actor:get_animation()
                 anim:set_state("ATTACK")
                 anim:set_playback(Playback.Once)
-                action.attack_anim_started = true
-                local direction = actor:get_facing()
-                local warning_duration = 0.5
-                local fire_duration = 0.5
-                local fire_damage = 200
-                for i = 1, 9, 1 do
-                    local t1 = actor:get_tile(direction,i)
-                    if t1 then
-                        fire_tower_spell(actor,fire_damage,fire_duration,warning_duration,t1:x(),t1:y())
+                action.pre_attack_anim_started = true
+            end
+            if action.pre_attack_time_counter < action.pre_attack_time then
+                action.pre_attack_time_counter = action.pre_attack_time_counter + dt
+
+                --flash target tiles
+                for index, target_tile in ipairs(action.target_tiles) do
+                    if action.warning_toggle then
+                        target_tile:highlight(Highlight.Solid)
                     end
                 end
-            end
-            if action.attack_time_counter < action.attack_time then
-                action.attack_time_counter = action.attack_time_counter + dt
+                --cycle flashing
+                action.warning_toggle_frames_elapsed = action.warning_toggle_frames_elapsed + 1
+                if action.warning_toggle_frames_elapsed >= action.warning_toggle_frames then
+                    action.warning_toggle_frames_elapsed = 0
+                    action.warning_toggle = not action.warning_toggle
+                end
+
+                --enable counter frames at certain time before attack
+                if action.pre_attack_time_counter <= action.pre_attack_time - action.pre_attack_counter_time and not action.counter_enabled then
+                    actor:toggle_counter(true)
+                end
             else
-                debug_print('action '..action_name..' step 1 complete')
-                local anim = actor:get_animation()
-                anim:set_state("IDLE")
-                anim:set_playback(Playback.Loop)
+                actor:toggle_counter(false)
                 self:complete_step()
             end
         end
+
         step2.update_func = function(self, dt)
-            --debug_print('action '..action_name..' step 2')
-            if not action.attack_anim_2_started then
-                local anim = actor:get_animation()
-                anim:set_state("ATTACK")
-                anim:set_playback(Playback.Once)
-                action.attack_anim_2_started = true
-                local direction = actor:get_facing()
-                local warning_duration = 0.5
-                local fire_duration = 0.5
-                local fire_damage = 200
-                for i = 1, 9, 1 do
-                    local t1 = actor:get_tile(direction,i)
-                    if t1 then
-                        fire_tower_spell(actor,fire_damage,fire_duration,warning_duration,t1:x(),t1:y()-1)
-                        fire_tower_spell(actor,fire_damage,fire_duration,warning_duration,t1:x(),t1:y()+1)
-                    end
-                end
-            end
-            if action.attack_time_2_counter < action.attack_2_time then
-                action.attack_time_2_counter = action.attack_time_2_counter + dt
-            else
-                debug_print('action '..action_name..' step 2 complete')
-                local anim = actor:get_animation()
-                anim:set_state("IDLE")
-                anim:set_playback(Playback.Loop)
-                self:complete_step()
-            end
-        end
-
-        self:add_step(step1)
-        if enraged then
-            self:add_step(step2)
-        end
-    end
-    return action
-end
-
-function action_hellfire(character)
-    local action_name = "action_hellfire"
-    debug_print('action '..action_name)
-
-    local action = Battle.CardAction.new(character,"IDLE")
-    action:set_lockout(make_sequence_lockout())
-
-    action.execute_func = function(self)
-        debug_print('executing action '..action_name)
-        local step1 = Battle.Step.new()
-
-        local actor = self:get_actor()
-        action.attack_anim_started = false
-        action.attack_time_counter = 0
-        action.time_till_next_fire = 0
-        action.waves = 3
-        action.delay_between_waves = 1
-        if enraged then
-            action.waves = 5
-            action.delay_between_waves = 0.8
-        end
-        action.attack_time = (action.waves*action.delay_between_waves)+1.5
-
-        step1.update_func = function(self, dt)
-            --debug_print('action '..action_name..' step 2')
-            if action.time_till_next_fire <= 0 and action.waves > 0 then
-                local anim = actor:get_animation()
-                anim:set_state("ATTACK")
-                anim:set_playback(Playback.Once)
+            -- debug_print('action '..action_name..' step 2')
+            if not action.attack_anim_started then
                 action.attack_anim_started = true
-                local direction = actor:get_facing()
-                local warning_duration = 1
-                local fire_duration = 0.5
-                local fire_damage = 200
-                local current_tile_x = actor:get_current_tile():x()
-                local field = actor:get_field()
-                for x = 0, current_tile_x, 1 do
-                    for y = 0, 4, 1 do
-                        if math.random(1,3) == 3 then
-                            local t1 = field:tile_at(x,y)
-                            if t1 then
-                                fire_tower_spell(actor,fire_damage,fire_duration,warning_duration,t1:x(),t1:y())
-                            end
-                        end
-                    end
+                Engine.play_audio(fire_tower_sound, AudioPriority.Highest)
+
+                --Do attacking
+                for index, target_tile in ipairs(action.target_tiles) do
+                    fire_tower_spell(character, damage, 0.5, target_tile:x(), target_tile:y())
                 end
-                action.time_till_next_fire = action.delay_between_waves
-                action.waves = action.waves - 1
-            else
-                action.time_till_next_fire = action.time_till_next_fire - dt
             end
             if action.attack_time_counter < action.attack_time then
                 action.attack_time_counter = action.attack_time_counter + dt
             else
-                debug_print('action '..action_name..' step 2 complete')
+                debug_print('action ' .. action_name .. ' step 2 complete')
                 local anim = actor:get_animation()
                 anim:set_state("IDLE")
                 anim:set_playback(Playback.Loop)
@@ -301,92 +285,94 @@ function action_hellfire(character)
         end
 
         self:add_step(step1)
+        self:add_step(step2)
     end
     return action
 end
 
-function fire_tower_spell(user,damage,duration,warning_duration,x,y)
+function spawn_visual_artifact(tile,character,texture,animation_path,animation_state,position_x,position_y)
+    local field = character:get_field()
+    local visual_artifact = Battle.Artifact.new(character:get_team())
+    visual_artifact:set_texture(texture,true)
+    local anim = visual_artifact:get_animation()
+    anim:load(animation_path)
+    anim:set_state(animation_state)
+    anim:on_complete(function()
+        visual_artifact:delete()
+    end)
+    visual_artifact:sprite():set_position(position_x,position_y)
+    field:spawn(visual_artifact, tile:x(), tile:y())
+end
+
+function fire_tower_spell(user, damage, duration, x, y)
     local field = user:get_field()
-    if x == 0 or x >= field:width()+1 or y == 0 or y >= field:height()+1 then
+    local target_tile = field:tile_at(x,y)
+    if target_tile:is_edge() then
         return
     end
     local spell = Battle.Spell.new(user:get_team())
     spell:set_texture(fire_tower_texture, true)
-    spell:highlight_tile(Highlight.Flash)
-    spell:set_hit_props(
-        make_hit_props(
-            damage, 
-            Hit.Impact | Hit.Flinch, 
-            Element.Fire, 
-            user:get_id(), 
-            drag(Direction.Right, 0)
-        )
-    )
+    spell:set_hit_props(make_hit_props(damage, Hit.Impact | Hit.Flash | Hit.Flinch,
+                                       Element.Fire, user:get_id(),
+                                       drag(Direction.Right, 0)))
     spell.elapsed = 0
     spell.current_state = 1
-    spell.state_changed = false
+    spell.state_changed = true
 
-    spell.duration_states = {
-        warning_duration,
-        0.15,
-        duration,
-        0.15,
-        999
-    }
+    spell.duration_states = {999, duration, 999, 999}
 
-    spell.update_func = function(self,delta_time)
+    spell.attack_func = function(self, other)
+        local tile = self:get_current_tile()
+        --TODO replace this with volcano effect (gotta make the animation)
+        spawn_visual_artifact(tile,self,impacts_texture,impacts_animation_path,"VOLCANO",0,0)
+    end
+
+    spell.update_func = function(self, delta_time)
+        -- damage entities
+        local current_tile = self:get_current_tile()
+        current_tile:attack_entities(self)
+        -- update elapsed time
         self.elapsed = self.elapsed + delta_time
         if self.elapsed >= self.duration_states[spell.current_state] then
             self.current_state = self.current_state + 1
             self.state_changed = true
             self.elapsed = 0
         end
+        --on state change
         if self.state_changed then
             local anim = self:get_animation()
-            if self.current_state == 2 then
-                spell:sprite():show()
+            if self.current_state == 1 then
                 anim:set_state("START")
                 anim:set_playback(Playback.Once)
-                anim:on_complete(function ()
-                    print("anim finished!")
+                anim:on_complete(function()
                     self.current_state = self.current_state + 1
                     self.state_changed = true
                     self.elapsed = 0
                 end)
-                self:highlight_tile(Highlight.None)
             end
-            if self.current_state == 3 then
+            if self.current_state == 2 then
                 anim:set_state("LOOP")
                 anim:set_playback(Playback.Loop)
             end
-            if self.current_state == 4 then
+            if self.current_state == 3 then
                 anim:set_state("END")
                 anim:set_playback(Playback.Once)
-                anim:on_complete(function ()
-                    print("anim finished! like totally")
+                anim:on_complete(function()
                     self.current_state = self.current_state + 1
                     self.state_changed = true
                     self.elapsed = 0
                 end)
             end
-            if self.current_state == 5 then
-                debug_print('spell complete')
+            if self.current_state == 4 then
                 spell:delete()
             end
             self.state_changed = false
         end
-        if self.current_state >= 2 then
-            --if we are in damaging frames
-            local current_tile = self:get_current_tile()
-            current_tile:attack_entities(self)
-        end
     end
 
     local anim = spell:get_animation()
-    spell:sprite():hide()
     anim:load(fire_tower_animation_path)
     anim:set_state("START")
-
     field:spawn(spell, x, y)
-    --use direct hit / back of field animation
+    return spell
 end
